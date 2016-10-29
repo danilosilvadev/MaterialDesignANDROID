3ec1c7bccd8908f206b0f3d6b841e7af0df5b5c6
GorickUPM <danilosilvaweb@hotmail.com>
2016-10-29 19:50:00 -0200

    popup

--- PopUP/app/src/main/java/study/popup/MainActivity.java
+++ PopUP/app/src/main/java/study/popup/MainActivity.java
@@ -2,56 +2,30 @@ package study.popup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     //Button
     private Button pesquisar;
-    private PopupWindow popUpWindowPesquisa;
-    private LayoutInflater layoutInflaterPesquisa;
-    private RelativeLayout relativeLayoutPesquisa;
-
-    private static final String TAG = "QuickNotesMainActivity";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Button pesquisar and popup
         pesquisar = (Button) findViewById(R.id.pesquisar);
-        relativeLayoutPesquisa = (RelativeLayout) findViewById(R.id.activity_pop_up);
+
         pesquisar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Log.e(TAG, "Begin");
-                layoutInflaterPesquisa = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-                //ViewGroup containerPesquisa = (ViewGroup) layoutInflaterPesquisa.inflate(R.layout.activity_pop_up, null);
-
-                PopupWindow pw = new PopupWindow(layoutInflaterPesquisa.inflate(R.layout.activity_pop_up, null, false),100,100, true);
-
-               // popUpWindowPesquisa = new PopupWindow(containerPesquisa, 600, 800, true);
-                popUpWindowPesquisa.showAtLocation(relativeLayoutPesquisa, Gravity.NO_GRAVITY, 500, 500);
-                relativeLayoutPesquisa.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent motionEvent) {
-                        Log.e(TAG, "End");
-                        popUpWindowPesquisa.dismiss();
-                        return true;
-
-                    }
-                });
+            public void onClick(View view) {
+                PopUp popUp = new PopUp();
+                popUp.show(getSupportFragmentManager(),"MyPopUp");
             }
         });
+
+
 
     }
 }
--- PopUP/app/src/main/java/study/popup/PopUp.java
+++ PopUP/app/src/main/java/study/popup/PopUp.java
@@ -1,13 +1,28 @@
 package study.popup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
-public class PopUp extends AppCompatActivity {
+public class PopUp extends DialogFragment {
+
+    LayoutInflater inflater;
+    View v;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pop_up);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        inflater = getActivity().getLayoutInflater();
+
+        v = inflater.inflate(R.layout.activity_pop_up, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(v);
+
+
+        return builder.create();
     }
+
 }
